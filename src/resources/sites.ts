b8b9b59391a24a03fbd1fb104f11cb1ca69a4a39
Amin Nausin <83550431+aminnausin@users.noreply.github.com>
2025-02-12 18:43:22 -0500

    Revert "new sites added"

--- src/resources/sites.ts
+++ src/resources/sites.ts
@@ -7,17 +7,5 @@ const sites: Website[] = [
         link: new URL('https://fareenkhan.vercel.app/'),
         skills: ['java', 'swift'],
     },
-    {
-        name: 'Zakir Dawood',
-        year: '2025',
-        link: new URL('https://zakirdawood.herokuapp.com/'),
-        skills: ['python', 'power bi'],
-    },
-    {
-        name: 'Muyeed Asshole',
-        year: '2025',
-        link: new URL('https://muyeed10.github.io/Portfolio/'),
-        skills: ['js', 'smalltalk'],
-    },
 ];
 export default sites;
